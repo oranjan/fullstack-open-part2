7ea801e6ae3b6bc5fd644fb4ce4e6647067d47f3
oranjan <rnjnmhta@gmail.com>
2026-01-02 17:08:39 +0530

    till ex2.5 done

--- ex2.1-2.5/src/components/Content.jsx
+++ ex2.1-2.5/src/components/Content.jsx
@@ -3,11 +3,9 @@ import Part from "./Part";
 export default function Content({ parts }) {
   return (
     <>
-      {parts.map((part) => {
-        return (
-          <Part key={part.id} part={part.name} exercise={part.exercises} />
-        );
-      })}
+      {parts.map((part) => (
+        <Part key={part.id} part={part.name} exercise={part.exercises} />
+      ))}
     </>
   );
 }
--- ex2.1-2.5/src/components/Header.jsx
+++ ex2.1-2.5/src/components/Header.jsx
@@ -1,3 +1,3 @@
 export default function Header(props) {
-  return <h1>{props.course}</h1>;
+  return <h2>{props.course}</h2>;
 }
